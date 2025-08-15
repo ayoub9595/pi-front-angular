6e332926e468d3e3728606585a2139a16bc7263d
abadia <abadia@norsys.fr>
2025-08-15 17:51:46 +0100

    Handle delete affectation

--- src/app/affectations/services/affectation.service.ts
+++ src/app/affectations/services/affectation.service.ts
@@ -8,29 +8,32 @@ import {AuthenticationService} from '../../services/authentication.service';
 })
 export class AffectationService {
 
-  apiURl = 'http://localhost:5001/affectations';
+  apiUrl = 'http://localhost:5001/affectations';
 
   constructor(private http: HttpClient,private authenticationService: AuthenticationService) { }
 
   getAllAffectations() {
     const isAdmin = this.authenticationService.isAdmin();
     if(isAdmin) {
-      return this.http.get<Affectation[]>(`${this.apiURl}/`)
+      return this.http.get<Affectation[]>(`${this.apiUrl}/`)
     }
     else {
       const id = this.authenticationService.getCurrentUserId();
-      return this.http.get<Affectation[]>(`${this.apiURl}/utilisateur/${id}`)
+      return this.http.get<Affectation[]>(`${this.apiUrl}/utilisateur/${id}`)
     }
 
   }
   addAffectation(affectation: AffectationForPersist) {
-    return this.http.post<Affectation>(`${this.apiURl}/`,affectation)
+    return this.http.post<Affectation>(`${this.apiUrl}/`,affectation)
   }
   getAffectationById(id: number) {
-    return this.http.get<Affectation>(`${this.apiURl}/${id}`)
+    return this.http.get<Affectation>(`${this.apiUrl}/${id}`)
   }
   updateAffectation(id:number,affectation: AffectationForPersist) {
-    return this.http.put<Affectation>(`${this.apiURl}/${id}`,affectation)
+    return this.http.put<Affectation>(`${this.apiUrl}/${id}`,affectation)
+  }
+  deleteAffectationById(id:number) {
+    return this.http.delete<{msg: string }>(`${this.apiUrl}/${id}`)
   }
 
 }
--- src/app/affectations/views/affectation-list/affectation-list.component.ts
+++ src/app/affectations/views/affectation-list/affectation-list.component.ts
@@ -17,6 +17,8 @@ export class AffectationListComponent implements OnInit {
   showInfoModal = false;
   selectedAffectation: Affectation | null = null;
   isAdmin: boolean = false;
+  showModal = false;
+  affectationIdToDelete: number| null = null;
 
   constructor(
     private affectationService: AffectationService,
@@ -60,7 +62,33 @@ export class AffectationListComponent implements OnInit {
   }
 
   handleDelete(id: number) {
+    this.affectationIdToDelete = id;
+    this.showModal = true;
+  }
 
+  deleteAffectation() {
+    this.affectationService.deleteAffectationById(this.affectationIdToDelete!).subscribe({
+      next: ({msg:message}) => {
+        console.log(message)
+        const index = this.affectations.findIndex(aff => aff.id === this.affectationIdToDelete!);
+        if(index >= -1) {
+          this.affectations.splice(index, 1);
+          this.affectationIdToDelete =  null
+          this.showModal = false;
+          this.toast.showToast(message,'success');
+        }
+      },
+      error: error => {
+        const errorMessage = error.error.message || 'Une erreur est survenue.';
+        this.showModal = false;
+        this.affectationIdToDelete =  null;
+        this.toast.showToast(errorMessage,'error');
+      }
+    })
+  }
+
+  closeModal() {
+    this.showModal = false;
   }
 
 }
