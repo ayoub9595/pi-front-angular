ca2b0238bd9ad89388099e2ce884e4f3ac0a0138
abadia <abadia@norsys.fr>
2025-08-15 18:09:44 +0100

    Add an animation to the toaster

--- src/app/components/toaster/toaster.component.ts
+++ src/app/components/toaster/toaster.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {ToasterService} from '../../services/toaster.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ToasterService } from '../../services/toaster.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'toaster',
@@ -7,19 +8,68 @@ import {ToasterService} from '../../services/toaster.service';
   templateUrl: './toaster.component.html',
   styleUrl: './toaster.component.css'
 })
-export class ToasterComponent implements OnInit{
+export class ToasterComponent implements OnInit, OnDestroy {
   message = '';
   className = '';
+  isVisible = false;
+  animationState = '';
+
+  private subscriptions = new Subscription();
 
   constructor(private toasterService: ToasterService) {}
 
   ngOnInit(): void {
-    this.toasterService.message$.subscribe(msg => this.message = msg);
-    this.toasterService.className$.subscribe(cls => this.className = cls);
+    // Subscribe to message changes
+    this.subscriptions.add(
+      this.toasterService.message$.subscribe(msg => {
+        if (msg && msg !== this.message) {
+          this.showToast(msg);
+        } else if (!msg) {
+          this.hideToast();
+        }
+        this.message = msg;
+      })
+    );
+
+    // Subscribe to className changes
+    this.subscriptions.add(
+      this.toasterService.className$.subscribe(cls => this.className = cls)
+    );
   }
 
-  close() {
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private showToast(message: string): void {
+    this.isVisible = true;
+    this.animationState = 'entering';
+
+    // Remove entering class after animation completes
+    setTimeout(() => {
+      this.animationState = '';
+    }, 400);
+  }
+
+  private hideToast(): void {
+    this.animationState = 'exiting';
+
+    // Hide component after exit animation completes
+    setTimeout(() => {
+      this.isVisible = false;
+      this.animationState = '';
+    }, 400);
+  }
+
+  close(): void {
     this.toasterService.hideToast();
   }
 
+  // Get combined CSS classes
+  getContainerClasses(): string {
+    const classes = ['container'];
+    if (this.className) classes.push(this.className);
+    if (this.animationState) classes.push(this.animationState);
+    return classes.join(' ');
+  }
 }
